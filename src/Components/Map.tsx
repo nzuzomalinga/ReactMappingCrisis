bc91d71e5e54fb18da348d4a7225b80235f508c4
Nzuzo_Malinga <nzuzomal@gmail.com>
2022-03-01 23:44:45 +0200

    Implementing osm place search library

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,19 +1,33 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Audio } from 'react-loader-spinner'
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 import '../Styles/Components/Maps.scss'
 
+export const Map = ({ location }: any) => {
+  
+const provider = new OpenStreetMapProvider();
 
-export const Map = ( {location} :any) => {
+const results = provider.search({ query: "postnet johannesburg" });
+
   return (
-    <MapContainer center={[location.lat,location.lng]}zoom={20}>
+
+    location.loaded ? <MapContainer center={[location.coordinates.lat, location.coordinates.lng]} zoom={20}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[location.lat,location.lng]}>
+      <Marker position={[location.coordinates.lat, location.coordinates.lng]}>
         <Popup>
           You are Here. <br /> Easily customizable.
         </Popup>
       </Marker>
-    </MapContainer>
+    </MapContainer> : <div className='loading-screen'>
+      <Audio
+        height="100"
+        width="100"
+        color='grey'
+        ariaLabel='loading'
+      />
+    </div>
   )
 }
