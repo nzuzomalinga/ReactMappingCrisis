cf72c3ed2ac22741888ae8a887f7da7485c83d45
Nzuzo_Malinga <nzuzomal@gmail.com>
2022-03-01 19:02:01 +0200

    Typescript migration

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -2,9 +2,9 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import '../Styles/Components/Maps.scss'
 
 
-export const Map = ( {location}) => {
+export const Map = ( {location} :any) => {
   return (
-    <MapContainer center={[location.lat,location.lng]} zoom={13}>
+    <MapContainer center={[location.lat,location.lng]}zoom={20}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
